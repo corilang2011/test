517a647999b392266a43f4fbd45adc84c5f79f88
Rico Langue <corilang2011@gmail.com>
2019-10-09 18:47:41 +0800

    Added p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
             <div class="card">
                 <div class="card-header">
                     <h4>Login 1</h4>
+                    <p>rico added this</p>
                 </div>
                 <div class="card-body">
                     
